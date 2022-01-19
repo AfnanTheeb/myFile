e1c3466033ac35cb744256dd463906b242870ba1
Njoud Alrshidi <nujud.abdulaziz.has.s@tuwaiq.edu.sam>
2022-01-19 09:44:27 +0300

    Done

--- Testone/majed.swift
+++ Testone/majed.swift
@@ -9,8 +9,8 @@ import Foundation
 
 struct Person{
     var name:String
-    var age:Int
+    
 }
 
-let person = Person(name: "Ahmed", age: 24)
-let newObj = Person(name: "Hamad", age: 25)
+let person = Person(name: "Ahmed")
+let newObj = Person(name: "Hamad")
